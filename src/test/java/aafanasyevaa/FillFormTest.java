3a8e427ae8efa1f587e32f198cedb9489b47b5cc
aafanasyevaa <afanasyevaa.anastasiia@gmail.com>
2021-11-05 18:47:42 +0300

    added build.gradle and .gitignore

---
+++ src/test/java/aafanasyevaa/FillFormTest.java
@@ -0,0 +1,18 @@
+package aafanasyevaa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class FillFormTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void FillFormTest() {
+
+    }
+}
