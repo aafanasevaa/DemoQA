3854480445ca6a202d562929c1338a4b22b9164d
aafanasyevaa <afanasyevaa.anastasiia@gmail.com>
2021-11-05 23:31:03 +0300

    updated the test

--- src/test/java/aafanasyevaa/pages/RegistrationPage.java
+++ src/test/java/aafanasyevaa/pages/RegistrationPage.java
@@ -16,37 +16,101 @@ public class RegistrationPage {
             firstNameInput = $("#firstName"),
             lastNameInput = $("#lastName"),
             emailInput = $("#userEmail"),
-            genderChoice = $(byText("Female")),
+            genderRadio = $(byText("Female")),
             numberInput = $("#userNumber"),
+//           calendarSelect = $("#dateOfBirthInput"),
+            subjectsInput = $("#subjectsInput"),
+            subjectSelect = $(byText("Chemistry")),
+            hobbySelect = $(byText("Sports")),
+            addressInput = $("#currentAddress"),
+            listOfStates = $("#state"),
+            stateSelect = $(byText("NCR")),
+            listOfCities = $("#city"),
+            citySelect = $(byText("Delhi")),
+            formSubmitButton = $("#submit");
+
+    public CalendarComponent calendar = new CalendarComponent();
+
 
     //actions
-    public RegistrationPage openPage (){
+    public RegistrationPage openPage() {
         open("https://demoqa.com/automation-practice-form");
         formTitle.shouldHave(text(FORM_TITLE));
         return this;
     }
 
-    public RegistrationPage typeFirstName (String name){
-       firstNameInput.setValue(name);
-       return this;
+    public RegistrationPage typeFirstName(String name) {
+        firstNameInput.setValue(name);
+        return this;
     }
 
-    public RegistrationPage typeLastName (String lastName){
+    public RegistrationPage typeLastName(String lastName) {
         lastNameInput.setValue(lastName);
         return this;
     }
 
-    public RegistrationPage typeEmail (String email){
+    public RegistrationPage typeEmail(String email) {
         emailInput.setValue(email);
         return this;
     }
 
-//    public void chooseGender (){
-//        genderChoice.click();
+    public RegistrationPage chooseGender() {
+        genderRadio.click();
+        return this;
+    }
+
+    public RegistrationPage typeNumber(String number) {
+        numberInput.setValue(number);
+        return this;
+    }
+
+//    public RegistrationPage openCalendar() {
+//        calendarSelect.click();
+//        return this;
 //    }
 
-    public RegistrationPage typeNumber (String number){
-        numberInput.setValue(number);
+    public RegistrationPage chooseSubject(String letter) {
+        subjectsInput.setValue(letter);
+        return this;
+    }
+
+    public RegistrationPage selectSubject() {
+        subjectSelect.click();
+        return this;
+    }
+
+    public RegistrationPage chooseHobby() {
+        hobbySelect.click();
+        return this;
+    }
+
+    public RegistrationPage typeAddress(String address) {
+        addressInput.setValue(address);
+        return this;
+    }
+
+    public RegistrationPage openListOfStates() {
+        listOfStates.click();
+        return this;
+    }
+
+    public RegistrationPage selectState() {
+        stateSelect.click();
+        return this;
+    }
+
+    public RegistrationPage openListOfCities() {
+        listOfCities.click();
+        return this;
+    }
+
+    public RegistrationPage selectCity() {
+        citySelect.click();
+        return this;
+    }
+
+    public RegistrationPage submitTheForm() {
+        formSubmitButton.click();
         return this;
     }
 }
--- src/test/java/aafanasyevaa/tests/FillFormTest.java
+++ src/test/java/aafanasyevaa/tests/FillFormTest.java
@@ -1,5 +1,6 @@
 package aafanasyevaa.tests;
 
+import aafanasyevaa.pages.RegistrationPage;
 import org.junit.jupiter.api.Test;
 
 public class FillFormTest extends TestBase {
@@ -10,29 +11,34 @@ public class FillFormTest extends TestBase {
                 .typeFirstName("Anastasiia")
                 .typeLastName("Afanasyeva")
                 .typeEmail("aaa@aaa")
-//                .chooseGender()
-                .typeNumber(888999777)
-//        $("#dateOfBirthInput").click();
+                .chooseGender()
+                .typeNumber("888999000777")
+//                .openCalendar()
+//                registrationPage.calendar.setDate("30", "July", "2008");
+
 //        $(".react-datepicker__month-select").selectOptionByValue(String.valueOf(11));
 //        $(".react-datepicker__year-select").selectOptionByValue(String.valueOf(1997));
 //        $$(".react-datepicker__day").find(text("1")).click();
 //        $("#subjectsInput").click();
-        $("#subjectsInput").setValue("C");
-        $(byText("Chemistry")).click();
-        $(byText("Sports")).click();
-        File image = new File("src/test/resources/pic.jpg");
-        $("#uploadPicture").uploadFile(image);
-        $("#currentAddress").setValue("Russia");
-        $("#state").click();
-        $(byText("NCR")).click();
-        $("#city").click();
-        $(byText("Delhi")).click();
-        $("#submit").click();
 
-        $(".table-responsive").shouldHave(text("Anastasiia"), text("Afanasyeva"), text("afanasyevaa.anastasiia@gmail.com"),
-                text("Female"), text("8899776655"), text("01 December ,1997"), text("Chemistry"),
-                text("Sports"), text("pic.jpg"), text("Russia"),
-                text("NCR Delhi"));
+                .chooseSubject("C")
+                .selectSubject()
+                .chooseHobby()
+
+//        File image = new File("src/test/resources/pic.jpg");
+//        $("#uploadPicture").uploadFile(image);
+
+                 .typeAddress("Russia")
+                 .openListOfStates()
+                 .selectState()
+                 .openListOfCities()
+                 .selectCity()
+                 .submitTheForm();
+
+//        $(".table-responsive").shouldHave(text("Anastasiia"), text("Afanasyeva"), text("afanasyevaa.anastasiia@gmail.com"),
+//                text("Female"), text("8899776655"), text("01 December ,1997"), text("Chemistry"),
+//                text("Sports"), text("pic.jpg"), text("Russia"),
+//                text("NCR Delhi"));
 
     }
 }
